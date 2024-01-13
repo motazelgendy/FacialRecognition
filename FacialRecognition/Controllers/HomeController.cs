c695fba4b3b85457a3527c888f3658d2d9cc0950
Motaz Elgendy <motazelgendy@outlook.com>
2024-01-13 02:15:06 +0200

    Update Train Model , Add test Samples

--- FacialRecognition/Controllers/HomeController.cs
+++ FacialRecognition/Controllers/HomeController.cs
@@ -6,12 +6,10 @@ using Emgu.CV.Face;
 using System.Threading.Tasks;
 using Emgu.CV.Util;
 using Emgu.CV.Reg;
-using System.Drawing;
 using Emgu.CV.CvEnum;
 using System;
-using static System.Net.Mime.MediaTypeNames;
 using System.Collections.Generic;
-using Microsoft.AspNetCore.Identity;
+
 
 namespace FacialRecognition.Controllers
 {
@@ -21,7 +19,7 @@ namespace FacialRecognition.Controllers
 	{
 
 		[HttpPut("MatchFaces")]
-		public async Task<IActionResult> MatchFaces(IFormFile RandomImage, IFormFile TrainImage, IFormFile PredictImage)
+		public async Task<IActionResult> MatchFaces(IFormFile FirstSample, IFormFile SecondSample, IFormFile PredictSample)
 		{
 			EigenFaceRecognizer recognizer = new EigenFaceRecognizer();
 			VectorOfMat TrainImageVecofMat = new VectorOfMat();
@@ -31,45 +29,41 @@ namespace FacialRecognition.Controllers
 			Labels.Add(1);
 			Labels.Add(2);
 
-			using var RandomStream = RandomImage.OpenReadStream();
-			using var ImageStream = TrainImage.OpenReadStream();
-			using var MemoryrandomStream = new MemoryStream();
-			using var MemoryTrainStream = new MemoryStream();
-			RandomStream.CopyTo(MemoryrandomStream);
-			ImageStream.CopyTo(MemoryTrainStream);
-			byte[] RandomBytes = MemoryrandomStream.ToArray();
-			byte[] TrainBytes = MemoryTrainStream.ToArray();
+			var FirstStream = FirstSample.OpenReadStream();
+			var SecondStream = SecondSample.OpenReadStream();
+			var MemoryFirstStream = new MemoryStream();
+			var MemorySecondStream = new MemoryStream();
+			FirstStream.CopyTo(MemoryFirstStream);
+			SecondStream.CopyTo(MemorySecondStream);
+			byte[] FirstTrainBytes = MemoryFirstStream.ToArray();
+			byte[] SecondTrainBytes = MemorySecondStream.ToArray();
 
-			Mat RandomMat = new Mat();
-			Mat TrainMat =new Mat();
-			CvInvoke.Imdecode(RandomBytes, ImreadModes.Color, RandomMat);
-			CvInvoke.Imdecode(TrainBytes, ImreadModes.Color, TrainMat);
-			Image<Gray, Byte> RandomEmguImage = RandomMat.ToImage<Gray, Byte>().Resize(500, 500, Inter.Cubic);
-			Image<Gray, Byte> TrainEmguImage = TrainMat.ToImage<Gray, Byte>().Resize(500, 500, Inter.Cubic);
+			Mat FirstTrainMat = new Mat();
+			Mat SecondTrainMat = new Mat();
+
+			CvInvoke.Imdecode(FirstTrainBytes, ImreadModes.Color, FirstTrainMat);
+			CvInvoke.Imdecode(SecondTrainBytes, ImreadModes.Color, SecondTrainMat);
+			Image<Gray, Byte> FitstTrainEmguImage = FirstTrainMat.ToImage<Gray, Byte>().Resize(500, 500, Inter.Cubic);
+			Image<Gray, Byte> SecondTrainEmguImage = SecondTrainMat.ToImage<Gray, Byte>().Resize(500, 500, Inter.Cubic);
 			List<Image<Gray, Byte>> TrainedFaces = new List<Image<Gray, Byte>>();
-			TrainedFaces.Add(TrainEmguImage);
-			TrainedFaces.Add(RandomEmguImage);
-		
-			
-
-
-
+			TrainedFaces.Add(FitstTrainEmguImage);
+			TrainedFaces.Add(SecondTrainEmguImage);
 			TrainImageVecofMat.Push(TrainedFaces.ToArray());
 			vectorOfInt.Push(Labels.ToArray());
 
 			recognizer.Train(TrainImageVecofMat, vectorOfInt);
 
-			using var PredictStream = PredictImage.OpenReadStream();
-			using var MemoryPredictStream = new MemoryStream();
+			 var PredictStream = PredictSample.OpenReadStream();
+			 var MemoryPredictStream = new MemoryStream();
 			PredictStream.CopyTo(MemoryPredictStream);
-			byte[] PredictBytes = MemoryrandomStream.ToArray();
+			byte[] PredictBytes = MemoryPredictStream.ToArray();
 			Mat PredictMat = new Mat();
 			CvInvoke.Imdecode(PredictBytes, ImreadModes.Color, PredictMat);
 
 			Image<Gray, byte> PredictEmguImage = PredictMat.ToImage<Bgr, byte>().Resize(500, 500, Inter.Cubic).Convert<Gray, byte>();
 			var result = recognizer.Predict(PredictEmguImage);
 
-			return result.Distance > 3000 && result.Label == 1  ? Ok(true) : Ok(false);
+			return result.Distance > 3000 ? Ok(true) : Ok(false);
 		
 
 
