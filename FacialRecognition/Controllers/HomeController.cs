c4efe7242c9541869514d7c18d3099aa832ea293
Motaz Elgendy <motazelgendy@outlook.com>
2024-01-23 22:58:09 +0200

    Add Face Detection to enhance Recognize

--- FacialRecognition/Controllers/HomeController.cs
+++ FacialRecognition/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ using Emgu.CV.Reg;
 using Emgu.CV.CvEnum;
 using System;
 using System.Collections.Generic;
-
+using System.Drawing;
 
 namespace FacialRecognition.Controllers
 {
@@ -18,16 +18,19 @@ namespace FacialRecognition.Controllers
 	public class HomeController : ControllerBase
 	{
 
+
 		[HttpPut("MatchFaces")]
 		public async Task<IActionResult> MatchFaces(IFormFile FirstSample, IFormFile SecondSample, IFormFile PredictSample)
 		{
+
+			string HaarCascadePath = Directory.GetCurrentDirectory() + @"\haarcascade_frontalface_alt.xml";
+			CascadeClassifier faceCasacdeClassifier = new CascadeClassifier(HaarCascadePath);
 			EigenFaceRecognizer recognizer = new EigenFaceRecognizer();
 			VectorOfMat TrainImageVecofMat = new VectorOfMat();
 			List<int> Labels = new List<int>();
-			
 			VectorOfInt vectorOfInt = new VectorOfInt();
+			Labels.Add(0);
 			Labels.Add(1);
-			Labels.Add(2);
 
 			var FirstStream = FirstSample.OpenReadStream();
 			var SecondStream = SecondSample.OpenReadStream();
@@ -41,12 +44,18 @@ namespace FacialRecognition.Controllers
 			Mat FirstTrainMat = new Mat();
 			Mat SecondTrainMat = new Mat();
 
+
 			CvInvoke.Imdecode(FirstTrainBytes, ImreadModes.Color, FirstTrainMat);
 			CvInvoke.Imdecode(SecondTrainBytes, ImreadModes.Color, SecondTrainMat);
-			Image<Gray, Byte> FitstTrainEmguImage = FirstTrainMat.ToImage<Gray, Byte>().Resize(500, 500, Inter.Cubic);
-			Image<Gray, Byte> SecondTrainEmguImage = SecondTrainMat.ToImage<Gray, Byte>().Resize(500, 500, Inter.Cubic);
+			Rectangle[] FirstImageFace = faceCasacdeClassifier.DetectMultiScale(FirstTrainMat, 1.1, 3, Size.Empty, Size.Empty);
+			Image<Gray, Byte> FitstTrainEmguImage = FirstTrainMat.ToImage<Gray, Byte>();
+			Image<Gray, Byte> SecondTrainEmguImage = SecondTrainMat.ToImage<Gray, Byte>().Resize(500, 500, Inter.Cubic).Convert<Gray, Byte>();
+			FitstTrainEmguImage.ROI = FirstImageFace[0];
+			Image<Gray, Byte> ROIFirstImage = FitstTrainEmguImage.Resize(500, 500, Inter.Cubic).Convert<Gray, Byte>();
+			ROIFirstImage.Save("C:\\Users\\motaz\\Desktop\\train.jpg");
+			FitstTrainEmguImage.Resize(500, 500, Inter.Cubic).Convert<Gray, Byte>();
 			List<Image<Gray, Byte>> TrainedFaces = new List<Image<Gray, Byte>>();
-			TrainedFaces.Add(FitstTrainEmguImage);
+			TrainedFaces.Add(ROIFirstImage);
 			TrainedFaces.Add(SecondTrainEmguImage);
 			TrainImageVecofMat.Push(TrainedFaces.ToArray());
 			vectorOfInt.Push(Labels.ToArray());
@@ -59,11 +68,15 @@ namespace FacialRecognition.Controllers
 			byte[] PredictBytes = MemoryPredictStream.ToArray();
 			Mat PredictMat = new Mat();
 			CvInvoke.Imdecode(PredictBytes, ImreadModes.Color, PredictMat);
+			Rectangle[] PredictImageFace = faceCasacdeClassifier.DetectMultiScale(PredictMat, 1.1, 3, Size.Empty, Size.Empty);
 
-			Image<Gray, byte> PredictEmguImage = PredictMat.ToImage<Bgr, byte>().Resize(500, 500, Inter.Cubic).Convert<Gray, byte>();
-			var result = recognizer.Predict(PredictEmguImage);
+			Image<Gray, byte> PredictEmguImage = PredictMat.ToImage<Gray, Byte>();
+				PredictEmguImage.ROI = PredictImageFace[0];
+			Image<Gray, Byte> ROIPredictImage = PredictEmguImage.Resize(500, 500, Inter.Cubic).Convert<Gray, Byte>();
+			ROIPredictImage.Save("C:\\Users\\motaz\\Desktop\\predict.jpg");
+			var result = recognizer.Predict(ROIPredictImage);
 
-			return result.Distance > 3000 ? Ok(true) : Ok(false);
+			return result.Label ==0 && result.Distance < 7000 ? Ok(true) : Ok(false);
 		
 
 
@@ -71,3 +84,4 @@ namespace FacialRecognition.Controllers
 
 	}
 }
+
